888a934002671d0d69599b59f5a01e897e9a1bce
tayri-tours <office@tayri-tours.co.il>
2025-05-30 03:10:34 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,22 +1,21 @@
-from flask import Flask, request
+בקשתָה,בקשתָהָ מֵבא יבואן מן קבּוק
+אַפּליקַציָה = בקבוק(__נָא__)
+אסימון אַימות = "tayriToken2025"  # <<< שים כאן את הטוקן בדיוק כמו שהגדרת ב-Meta
 
-app = Flask(__name__)
+@אַפּליקַציָה.route('/', שיטות=['GET'])
+def לאמת():
+    אסימון = בקשתָה.ארגומנטים.get('hub.verify_token')
+    אם אסימון == אסימון אַימות:
+        return בקשתָה.ארגומנטים.get('hub.challenge'), 200
+    return 'Error', 403
 
-VERIFY_TOKEN = "tayriToken2025"  # <<< שים כאן את אותו הטוקן בדיוק כמו שהזנת ב-Meta
-
-@app.route('/', methods=['GET'])
-def verify():
-    token = request.args.get('hub.verify_token')
-    challenge = request.args.get('hub.challenge')
-    if token == VERIFY_TOKEN:
-        return challenge, 200
-    return 'Invalid verification token', 403
-
-@app.route('/', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    print("📥 התקבלה הודעה:", data)
-    return 'OK', 200
+@אַפּליקַציָה.route('/', שיטות=['POST'])
+def לקלוט():
+    מידע = בקשתָה.get_json()
+    print("📩 קיבלתי מידע:", מידע)
+    return "קיבלתי", 200
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    האַפּליקַציָה.run(host='0.0.0.0', port=port)
