8c6a66e2278f8246f2558388e3b750ed3523d40a
tayri-tours <office@tayri-tours.co.il>
2025-05-30 03:22:59 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,21 +1,19 @@
-בקשתָה,בקשתָהָ מֵבא יבואן מן קבּוק
-אַפּליקַציָה = בקבוק(__נָא__)
-אסימון אַימות = "tayriToken2025"  # <<< שים כאן את הטוקן בדיוק כמו שהגדרת ב-Meta
+from flask import Flask, request
+import os
 
-@אַפּליקַציָה.route('/', שיטות=['GET'])
-def לאמת():
-    אסימון = בקשתָה.ארגומנטים.get('hub.verify_token')
-    אם אסימון == אסימון אַימות:
-        return בקשתָה.ארגומנטים.get('hub.challenge'), 200
+האַפּליקַציָה = Flask(__name__)
+
+אסימון_אימות = "tayriToken2025"
+
+@האַפּליקַציָה.route('/', methods=['GET'])
+def אימות():
+    אסימון = request.args.get('hub.verify_token')
+    if אסימון == אסימון_אימות:
+        return request.args.get('hub.challenge'), 200
     return 'Error', 403
 
-@אַפּליקַציָה.route('/', שיטות=['POST'])
-def לקלוט():
-    מידע = בקשתָה.get_json()
+@האַפּליקַציָה.route('/', methods=['POST'])
+def קליטה():
+    מידע = request.get_json()
     print("📩 קיבלתי מידע:", מידע)
-    return "קיבלתי", 200
-
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    האַפּליקַציָה.run(host='0.0.0.0', port=port)
+    return "Received", 200
