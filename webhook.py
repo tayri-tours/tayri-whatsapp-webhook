4350a9441f9e63e9ce6e7e37077116a03467a480
tayri-tours <office@tayri-tours.co.il>
2025-05-30 03:41:08 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -17,6 +17,3 @@ def webhook():
     data = request.get_json()
     print("📩 קיבלנו מידע:", data)
     return 'Received', 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
