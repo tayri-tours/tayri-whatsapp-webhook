519bad2fd72ddea7f4bfba4ae9c2c1eab9be0235
tayri-tours <office@tayri-tours.co.il>
2025-07-29 02:45:27 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -17,6 +17,6 @@ def webhook():
     data = request.get_json()
     print("📩 קיבלנו מידע:", data)
     return 'Received', 200
-if __name__ == "__main__":
+if __name__ == "__main__": 
     port = int(os.environ.get("PORT", 5000))  # ברנדר יכניס את הפורט הנכון
     app.run(host="0.0.0.0", port=port)
