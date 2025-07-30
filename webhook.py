1f68563cfda2aff30044779ca7d1b875923abe7f
tayri-tours <office@tayri-tours.co.il>
2025-07-31 02:33:23 +0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -29,4 +29,5 @@ def webhook():
     return 'Unsupported method', 405
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
